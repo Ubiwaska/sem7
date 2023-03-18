585797208fb8a49c7f67ae13e30155935458b447
Полина Галкина <polygal@inbox.ru>
2023-03-18 12:59:15 +0300

    added ex47

---
+++ 47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Enter the number of rows");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of columns");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+ double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble()*(maxValue - minValue) + minValue;;
+        }
+    }
+    return result;
+}
+ void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
